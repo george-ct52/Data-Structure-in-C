b1929e050c89e9a1c7f9c8d071f26bbb498aef32
George C Thomas <61283815+george-ct52@users.noreply.github.com>
2024-02-12 00:18:33 +0530

    Create polynomial addition.c

---
+++ polynomial addition.c
@@ -0,0 +1,138 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+    int coeff;
+    int expo;
+    struct Node* next;
+};
+
+typedef struct Node node;
+
+node *newnode()
+{
+    node *p;
+    p=(node*)malloc(sizeof(node));
+    return p;
+    
+}
+
+node *read()
+{
+    int coe,exp,ch;
+    node *curr, *head;
+    head=newnode();
+    curr=head;
+    do{
+        printf("Enter the coeffiecient :");
+        scanf("%d",&curr->coeff);
+        printf("Enter the exponent :");
+        scanf("%d",&curr->expo);
+        printf("More values ? (0,1)");
+        scanf("%d",&ch);
+        if(ch==1)
+        {
+            curr->next=newnode();
+            curr=curr->next;
+        }
+    }while (ch!=0);
+    curr->next =NULL;
+    return head;
+}
+        
+node *add(node *a , node *b)
+{
+    node *c,*curr;
+    c=newnode();
+    curr=c;
+    while(a!=NULL & b!=NULL)
+    {
+        if(a->expo==b->expo)
+        {
+            curr->expo=a->expo;
+            curr->coeff=a->coeff+b->coeff;
+            a=a->next;
+            b=b->next;
+        }
+        else if(a->expo > b->expo)
+        {
+            curr->expo=a->expo;
+            curr->coeff=a->coeff;
+        }
+        else
+        {
+            curr->expo=b->expo;
+            curr->coeff=b->coeff;
+        }
+        if(a!=NULL && b!= NULL)
+        {
+            curr->next=newnode();
+            curr=curr->next;
+        }
+    }
+    if(a!=NULL || b!= NULL)
+    {
+        curr->next=newnode();
+        curr=curr->next;
+    }
+    
+    while(a!=NULL)
+    {
+        curr->coeff=a->coeff;
+        curr->expo=a->expo;
+        a=a->next;
+        if(a!=NULL)
+        {
+            curr->next =newnode();
+            curr=curr->next;
+        }
+        
+    }
+    
+    while(b!=NULL)
+    {
+        curr->coeff=b->coeff;
+        curr->expo=b->expo;
+        b=b->next;
+        if(b!=NULL)
+        {
+            curr->next =newnode();
+            curr=curr->next;
+        }
+        
+    }
+    curr->next =NULL;
+    return c;
+    
+}        
+
+void display(node *a)
+{
+   node *curr=a;
+   printf("\n");
+   while(curr->next!=NULL)
+   {
+       printf("%d x^%d",curr->coeff,curr->expo);
+       if(curr->next!=NULL)
+       {
+           printf("+");
+       }
+       curr =curr->next;
+   }
+   printf("\n");
+}
+
+void main()
+{
+    node *a ,*b,*c;
+    printf("Enter the first polynomial :");
+    a=read();
+    printf("Enter the second polynomial :");
+    b=read();
+    display(a);
+    display(b);
+    c=add(a,b);
+    printf("The added polynomial is :");
+    display(c);
+}
